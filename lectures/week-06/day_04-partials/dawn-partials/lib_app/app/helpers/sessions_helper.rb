12c65a9768444a864fa31431270b68deafe899d2
Delmer Reed <delmer814@yahoo.com>
2015-06-11 09:25:18 -0700

    hello

---
+++ lectures/week-06/day_04-partials/dawn-partials/lib_app/app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+  
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    if current_user == nil
+      redirect_to "/login"
+    end
+  end
+
+  def logout
+    @current_user = session[:user_id] = nil
+  end
+
+end
---
+++ lectures/week-06/day_04-partials/dawn-partials/lib_app/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  def self.confirm(params)
+    user = User.find_by({email: params[:email]})
+    user.try(:authenticate, params[:password])
+  end
+end
